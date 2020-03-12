02c111c96b069225a565ff892dd0da60cd714359
Ilia <dotsenko.iv@edu.spbstu.ru>
2020-03-12 12:16:08 +0300

    Initial commit

---
+++ frontend/controllers/ClonController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace frontend\controllers;
+
+
+use yii\web\Controller;
+
+class ClonController extends Controller
+{
+    /**
+     * Displays homepage.
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
